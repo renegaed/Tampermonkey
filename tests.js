e45a86617c5089b067193d0c9c69e8846982acb3
Vijay Madas <renegaed@hotmail.com>
2015-02-22 15:08:53 +1100

    remove assertequals method

--- tests.js
+++ tests.js
@@ -5,30 +5,3 @@ window.My = window.My || {};
 if ( ! My.Utilities) {
 	console.log("Unable to load Test File. Missing Dependency: Utilities file");
 }
-
-/**
- * Tests Object
- * @type {Object}
- */
-My.Tests = {
-
-	/**
-	 * Create assert equals acceptance testing
-	 */
-	assertEquals: function(one, two) {
-    var test_status = "";
-    var color = "";
-    var output = "";
-
-    if (two === one) {
-        test_status = "PASSED";
-        color = "success";
-    } else {
-        test_status = "FAILED";
-        color = "error";
-    }
-
-    output = 'TEST {0}: "{1}" == "{2}"'.sprintf(test_status, two, one);
-
-    My.Utilities.log(output, color);
-};
