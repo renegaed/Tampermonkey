9d14eb56c76e951788a718d2f1fd4e736e808439
Vijay Madas <renegaed@hotmail.com>
2015-02-22 19:45:47 +1100

    update test file to support generic output method

--- tests.js
+++ tests.js
@@ -10,27 +10,107 @@ if ( ! My.Utilities) {
  * Tests Object
  * @type {Object}
  */
-My.Tests = {
+My.Test = {
 
 	/**
-	 * Create assert equals acceptance testing
+	 * Create assert equals test
+	 *
+	 * @param  {[mixed]} 	actual   the actual result
+	 * @param  {[type]} 	expected the expected result
+	 * @param  {[type]} 	msg      (optional) output message
 	 */
-	assertEquals: function(one, two) {
-    var test_status = "";
-    var color = "";
-    var output = "";
+	assertEquals: function(actual, expected, msg) {
+		var result;
 
-    if (two === one) {
-        test_status = "PASSED";
-        color = "success";
-    } else {
-        test_status = "FAILED";
-        color = "error";
-    }
+		if ( actual === expected ) {
+			result = true;
+		} else {
+			result = false;
+		}
 
-    output = 'TEST {0}: "{1}" == "{2}"'.sprintf(test_status, two, one);
+		this._testResult( "assertEquals", result, actual, expected, msg );
+	},
 
-    My.log(output, color);
-  }
+	/**
+	 * Create assert not equals test
+	 *
+	 * @param  {[mixed]} 	actual   the actual result
+	 * @param  {[type]} 	expected the expected result
+	 * @param  {[type]} 	msg      (optional) output message
+	 */
+	assertNotEquals: function(actual, expected, msg) {
+		var result;
+
+		if ( actual !== expected ) {
+			result = true;
+		} else {
+			result = false;
+		}
+
+		this._testResult( "assertNotEquals", result, actual, expected, msg );
+	},
+
+	/**
+	 * Output the result of the test
+	 *
+	 * @param  {string} 	name   		the name of the test which ran
+	 * @param  {bool} 		result   	whether test passed or failed
+	 * @param  {mixed} 		expected 	the expected result
+	 * @param  {mixed} 		actual   	the actual result
+	 * @param  {string}		msg				(optional) output message
+	 */
+	_testResult: function( name, result, expected, actual, msg ) {
+
+		var status = "";
+    	var color = "";
+    	var output = "";
+
+		if ( result ) {
+			status = "PASSED";
+			color = "success";
+		} else {
+			status = "FAILED";
+			color = "error";
+		}
+
+		// if output message exists,
+		// display and exit
+		if (typeof msg != "undefined") {
+			My.log(msg, color);
+			return;
+		}
+
+		switch (name) {
+			case "assertEquals":
+				output = 'TEST {0}: "{1}" == "{2}"'.sprintf( status, expected, actual );
+				break;
+
+			case "assertNotEquals":
+				output = 'TEST {0}: "{1}" != "{2}"'.sprintf( status, expected, actual );
+				break;
+
+			default:
+				// for output purposes,
+				// replace all undefined variables with the string "undefined"
+				for ( index in arguments ) {
+					if ( typeof arguments[index] == "undefined" ) {
+						arguments[index] = "undefined" //
+					}
+				}
+
+				output = 'TEST {0} with Arguments: name => "{1}", result => "{2}", expected => "{3}", actual => "{4}", msg => "{5}"'.sprintf(
+                    status,
+                    arguments[0],
+                    arguments[1],
+                    arguments[2],
+                    arguments[3],
+                    arguments[4]
+                );
+				break;
+		}
+
+		My.log(output, color);
+
+	},
 
 };
