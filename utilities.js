44e16149cece7357d3e6c6af43a9bc70a2ad2f2b
Vijay Madas <renegaed@hotmail.com>
2015-02-22 15:42:46 +1100

    update utility methods to extend direct off My Object

--- utilities.js
+++ utilities.js
@@ -2,36 +2,32 @@
 window.My = window.My || {};
 
 /**
- * Utilities Object
- * @type {Object}
+ * Utility Methods
  */
-My.Utilities = {
 
-	/**
-	 * Add color to console.log
-	 * @param mixed msg message to be output
-	 */
-	log: function(msg, color) {
-    color = color || "black";
-    bgc = "White";
-    switch (color) {
-        case "success":  color = "Green";      bgc = "#BEFAA4";       	break;
-        case "info":     color = "DodgerBlue"; bgc = "#C1F7FC";       	break;
-        case "error":    color = "Red";        bgc = "#FCC1C1";        	break;
-        case "warning":  color = "#FF9E0A";    bgc = "#FCF2C1";         break;
-        default: color = color;
-    }
-
-    if (typeof msg == "object"){
-        console.log(msg);
-    } else if (typeof color == "object"){
-        console.log("%c" + msg, "color: PowderBlue;font-weight:bold; background-color: RoyalBlue;");
-        console.log(color);
-    } else {
-        console.log("%c" + msg, "color:" + color + ";font-weight:bold; background-color: " + bgc + ";");
-    }
+/**
+ * Add color to console.log
+ * @param mixed msg message to be output
+ */
+My.log = function(msg, color) {
+  color = color || "black";
+  bgc = "White";
+  switch (color) {
+      case "success":  color = "Green";      bgc = "#BEFAA4";       	break;
+      case "info":     color = "DodgerBlue"; bgc = "#C1F7FC";       	break;
+      case "error":    color = "Red";        bgc = "#FCC1C1";        	break;
+      case "warning":  color = "#FF9E0A";    bgc = "#FCF2C1";         break;
+      default: color = color;
   }
 
+  if (typeof msg == "object"){
+      console.log(msg);
+  } else if (typeof color == "object"){
+      console.log("%c" + msg, "color: PowderBlue;font-weight:bold; background-color: RoyalBlue;");
+      console.log(color);
+  } else {
+      console.log("%c" + msg, "color:" + color + ";font-weight:bold; background-color: " + bgc + ";");
+  }
 };
 
 /**
