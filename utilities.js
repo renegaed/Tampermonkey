2461c1e0c490d05332f320f362fc4ebe83072a7f
Vijay Madas <renegaed@hotmail.com>
2015-03-01 18:37:57 +1100

    added a url object  with getQuerystring() method
    - added capital letter to utilities object
    -indentation changes

--- utilities.js
+++ utilities.js
@@ -4,7 +4,7 @@ window.My = window.My || {};
 /**
  * Utility Methods
  */
-My.Utilities = {
+ My.Utilities = {
 
 	//
 
@@ -14,25 +14,56 @@ My.Utilities = {
  * Add color to console.log
  * @param mixed msg message to be output
  */
-My.log = function(msg, color) {
+ My.log = function(msg, color) {
   color = color || "black";
   bgc = "White";
   switch (color) {
-      case "success":  color = "Green";      bgc = "#BEFAA4";       	break;
-      case "info":     color = "DodgerBlue"; bgc = "#C1F7FC";       	break;
-      case "error":    color = "Red";        bgc = "#FCC1C1";        	break;
-      case "warning":  color = "#FF9E0A";    bgc = "#FCF2C1";         break;
-      default: color = color;
+    case "success":  color = "Green";      bgc = "#BEFAA4";       	break;
+    case "info":     color = "DodgerBlue"; bgc = "#C1F7FC";       	break;
+    case "error":    color = "Red";        bgc = "#FCC1C1";        	break;
+    case "warning":  color = "#FF9E0A";    bgc = "#FCF2C1";         break;
+    default: color = color;
   }
 
   if (typeof msg == "object") {
-      console.log(msg);
+    console.log(msg);
   } else if (typeof color == "object"){
-      console.log("%c" + msg, "color: PowderBlue;font-weight:bold; background-color: RoyalBlue;");
-      console.log(color);
+    console.log("%c" + msg, "color: PowderBlue;font-weight:bold; background-color: RoyalBlue;");
+    console.log(color);
   } else {
-      console.log("%c" + msg, "color:" + color + ";font-weight:bold; background-color: " + bgc + ";");
+    console.log("%c" + msg, "color:" + color + ";font-weight:bold; background-color: " + bgc + ";");
   }
+};
+
+/**
+ * Create the url object for manipulating urls
+ * @type {Object}
+ */
+ My.Url = {
+
+  /**
+   * returns the query string by name
+   *
+   * usage: urlParams["name"]
+   * usage: "empty" in urlParams --> true/false
+   * 
+   * @param  {string}
+   */
+  getQuerystring: function( name ) {
+    var match,
+      pl     = /\+/g,  // Regex for replacing addition symbol with a space
+      search = /([^&=]+)=?([^&]*)/g,
+      decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+      query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query)) {
+      urlParams[decode(match[1])] = decode(match[2]);
+    }
+
+    return urlParams;
+  },
+
 };
 
 /**
@@ -43,12 +74,12 @@ My.log = function(msg, color) {
  * 	{2} -> third item to be replaced
  * 	etc.
  */
-String.prototype.sprintf = function() {
-	var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) {
-        return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-    });
-};
+ String.prototype.sprintf = function() {
+   var args = arguments;
+   return this.replace(/{(\d+)}/g, function(match, number) {
+    return typeof args[number] != 'undefined'
+    ? args[number]
+    : match
+    ;
+  });
+ };
