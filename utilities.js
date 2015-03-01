fe0cd48be89fafbf064805ca9b52a20e31e15088
Vijay Madas <renegaed@hotmail.com>
2015-03-01 18:57:28 +1100

    UpdateQueryString method

--- utilities.js
+++ utilities.js
@@ -41,7 +41,7 @@ window.My = window.My || {};
  */
  My.Url = {
 
-  _querystring: false,
+  _querystring: null,
 
   /**
    * returns the query string by name
@@ -51,11 +51,11 @@ window.My = window.My || {};
    * 
    * @param  {string}
    */
-  getQuerystring: function( name ) {
+   getQueryString: function( name ) {
 
     // if querystring has already been extracted
     // simply return that
-    if (this._querystring) {
+    if ( typeof this._querystring !== "undefined" && this._querystring !== null )) {
       return this._querystring;
     }
 
@@ -65,12 +65,63 @@ window.My = window.My || {};
       decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
       query  = window.location.search.substring(1);
 
-    this._querystring = {};
-    while (match = search.exec(query)) {
-      this._querystring[decode(match[1])] = decode(match[2]);
+      this._querystring = {};
+      while (match = search.exec(query)) {
+        this._querystring[decode(match[1])] = decode(match[2]);
+      }
+
+      return this._querystring;
+    },
+
+  /**
+   * replace/update/remove the querystring
+   * 
+   * 
+   * @param {string}  key   the querystring parameter in question
+   * @param {string}  value (optional) the querystring value
+   *                        supplying a value will add/update the parameter
+   *                        not supplying a value will remove the parameter
+   * @param {string}  url   (optional) the url to parse, defaults to window.location
+   */
+  UpdateQueryString: function( key, value, url ) {
+    if ( ! url ) {
+      url = window.location.href;
     }
 
-    return this._querystring;
+    var re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi"),
+    var hash;
+
+    if ( re.test( url ) ) {
+      if ( typeof value !== "undefined" && value !== null ) {
+        return url.replace(re, '$1' + key + "=" + value + '$2$3');
+      }
+      else {
+        hash = url.split('#');
+        url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
+
+        if ( typeof hash[1] !== 'undefined' && hash[1] !== null ) {
+          url += '#' + hash[1];
+        }
+
+        return url;
+      }
+    }
+    else {
+      if ( typeof value !== 'undefined' && value !== null ) {
+        var separator = url.indexOf('?') !== -1 ? '&' : '?';
+        hash = url.split('#');
+        url = hash[0] + separator + key + '=' + value;
+
+        if (typeof hash[1] !== 'undefined' && hash[1] !== null) {
+          url += '#' + hash[1];
+        }
+
+        return url;
+      }
+      else {
+        return url;
+      }
+    }
   },
 
 };
